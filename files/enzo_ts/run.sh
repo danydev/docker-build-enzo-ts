503dd31f3ae2e1198a138435ad87cde5d021908e
Daniele Orrù <daniele.orru.dev@gmail.com>
2020-07-04 13:33:43 +0200

    Build enzo-ts 1.3.12

--- files/enzo_ts/run.sh
+++ files/enzo_ts/run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-LD_LIBRARY_PATH=bin/deps bin/enzo_tsl
+lib_path="bin/deps_base"
 
+# NOTE: include the necessary additional dependencies
+if [ "`dirname \"$(whereis libGLU.so)\"`" == "." ];then
+    lib_path+=":bin/deps_gfx"
+fi
+if [ "`dirname \"$(whereis libssl.so)\"`" == "." ];then
+    lib_path+=":bin/deps_ssl"
+fi
+
+LD_LIBRARY_PATH="${lib_path}" bin/enzo_tsl
