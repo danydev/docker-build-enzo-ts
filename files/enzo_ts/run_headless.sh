135fbb958b87d5e8217b0b679069d7dd2c8439df
Daniele Orrù <daniele.orru.dev@gmail.com>
2021-07-27 09:50:45 +0200

    Build enzo-ts 3.0.1

--- files/enzo_ts/run_headless.sh
+++ files/enzo_ts/run_headless.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 BASHSCRIPTDIR=$(cd "$(dirname "$0")"; pwd)
-
-ENZO_LIBRARY_PATH="${BASHSCRIPTDIR}/bin:${BASHSCRIPTDIR}/bin/deps_base:${BASHSCRIPTDIR}/bin/deps_gfx:${BASHSCRIPTDIR}/bin/deps_ssl"
-
+ENZO_LIBRARY_PATH="${BASHSCRIPTDIR}/bin:${BASHSCRIPTDIR}/redistr"
 export LD_LIBRARY_PATH="${ENZO_LIBRARY_PATH}:${LD_LIBRARY_PATH}"
 
 bin/enzo_tsl_hl --low_memory_mode
